08d8778b31c0e7c72d7c06d48b69cf1b53aff86e
papasmf1 <papasmf1@gmail.com>
2021-11-02 14:29:06 +0900

    반복구문연습7

--- DemoLoop.py
+++ DemoLoop.py
@@ -37,3 +37,20 @@ for i in [2,3,4,5,6]:
     for j in [1,2,3,4,5,6,7,8,9]:
         print("{0} * {1} = {2}".format(i, j, i*j))
 
+#수열함수를 사용
+for i in range(1,11):
+    print("*" * i)
+
+
+lst = [1,2,3,4,5,6,7,8,9,10]
+for i in lst:
+    if i > 5:
+        break 
+    print("item:{0}".format(i))
+
+print("---continue---")
+for i in lst:
+    if i % 2 == 0:
+        continue 
+    print("item:{0}".format(i))
+
