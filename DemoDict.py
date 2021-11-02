cec4dcbf659a0241d0df402f55bd21c110ad1531
papasmf1 <papasmf1@gmail.com>
2021-11-02 10:17:59 +0900

    first commit

---
+++ DemoDict.py
@@ -0,0 +1,25 @@
+# DemoDict.py 
+device = {"아이폰":5, "아이패드":10, "윈도우":20}
+
+print(type(device))
+print( device )
+#입력 
+device["맥북"] = 15 
+#검색 
+print(device["아이폰"])
+#수정
+device["아이폰"] = 6 
+#삭제 
+del device["아이패드"]
+print( device )
+
+#참조를 복사
+device2 = device 
+print( device2 )
+device2["LG그램"] = 40 
+print("---참조를 복사---")
+print(device)
+print(device2)
+print( id(device), id(device2) )
+
+
