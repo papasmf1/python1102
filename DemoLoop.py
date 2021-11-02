30845fb495c00b78f6d29f9571ef3f08589a39d4
papasmf1 <papasmf1@gmail.com>
2021-11-02 13:42:18 +0900

    반복구문연습3

--- DemoLoop.py
+++ DemoLoop.py
@@ -12,6 +12,17 @@ while value < 10:
     print(value)
     value += 1 
 
+#갯수를 이미 알고 있는 경우
+lst = ["apple", 100, 3.14]
+print("갯수:", len(lst))
+for item in lst:
+    print(item, type(item))
+
+#딕셔너리
+d = {"apple":100, "kiwi":200, "orange":300}
+for item in d.items():
+    print(item)
+
 
 
 
