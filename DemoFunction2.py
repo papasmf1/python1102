e6acb5f701c58fe9b0945890414f3cf8c04b8937
papasmf1 <papasmf1@gmail.com>
2021-11-03 09:43:40 +0900

    람다함수까지연습2

--- DemoFunction2.py
+++ DemoFunction2.py
@@ -18,3 +18,33 @@ def connectURI(server, port):
 print( connectURI("credu.com", "80") )
 print( connectURI(port="80", server="credu.com") )
 
+#가변인자(갯수가 정해지지 않은 경우)
+def union(*ar):
+    #함수 내부에 글자를 모아둘 리스트를 초기화 
+    result = []
+    #HAM(0) | SPAM(1)
+    for item in ar:
+        #H(0) | A(1) | M(2)
+        for x in item:
+            #글자가 포함되어 있지 않으면 리스트에 추가
+            if x not in result:
+                result.append(x)
+    return result 
+
+#호출
+print( union("HAM","SPAM") )
+print( union("HAM","SPAM","EGG") )
+
+#정의되지 않은 인자:필수와 옵션이 있는 경우
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        strURL += key + "=" + user[key] + "&"
+    return strURL
+
+#호출
+print( userURIBuilder("credu.com","80", id="kim", passwd="1234") )
+print( userURIBuilder("credu.com","80", id="kim", passwd="1234",
+    name="mike", age="30"))
+    
+
