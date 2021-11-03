a91e98eb85e9e7e0bc5deb81fd43dadd2f199421
papasmf1 <papasmf1@gmail.com>
2021-11-03 11:25:43 +0900

    두번째 클래스 연습

--- class2.py
+++ class2.py
@@ -1,9 +1,12 @@
-# class1.py 
+# class2.py 
 #1)클래스 형식을 정의
 class Person:
+    #위치를 잘 본다~~
+    num_person = 0 
     #초기화메서드(생성자)
     def __init__(self):
         self.name = "default name"
+        Person.num_person += 1 
     #인스턴스 메서드
     def print(self):
         print("이름은 ", self.name)
@@ -12,9 +15,7 @@ class Person:
 p1 = Person()
 p2 = Person()
 
-#3)인스턴스 메서드 호출 
-p1.name = "전우치"
-p1.print()
-p2.print() 
+#인스턴스 갯수 확인
+print("몇개:", Person.num_person)
 
 
