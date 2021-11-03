40a90b416fe249418da59ff57cdfc9a57f16f73b
papasmf1 <papasmf1@gmail.com>
2021-11-03 09:51:24 +0900

    람다함수연습

--- DemoFunction2.py
+++ DemoFunction2.py
@@ -44,7 +44,14 @@ def userURIBuilder(server, port, **user):
 
 #호출
 print( userURIBuilder("credu.com","80", id="kim", passwd="1234") )
-print( userURIBuilder("credu.com","80", id="kim", passwd="1234",
+print( userURIBuilder("credu.com","80", id="kim", passwd="1234", 
     name="mike", age="30"))
-    
+
+#람다함수:이름이 없는 간결한 함수정의 문법
+g = lambda x,y:x*y
+print( g(3,4) )
+print( g(5,6) )
+
+print( (lambda x:x*x)(3) )
+print( globals() )
 
