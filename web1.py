8030d29577d1e6fa06c75aab360430cc733066ab
papasmf1 <papasmf1@gmail.com>
2021-11-04 16:12:24 +0900

    웹크롤링연습2

--- web1.py
+++ web1.py
@@ -1,11 +1,30 @@
 # web1.py 
 from bs4 import BeautifulSoup
 
-#페이지를 로딩 
+#페이지를 로딩 (유니코드를 지정): 연속적으로 메서드, 함수를 호출(메서드 체인)
 page = open("c:\\work\\test01.html", "rt", encoding="utf-8").read() 
-#검색이 용이한 객체
+#검색이 용이한 객체:정해진 상수 
 soup = BeautifulSoup(page, "html.parser")
-print( soup.prettify() )
+# print( soup.prettify() )
+
+#<p> 몽땅 검색
+# print( soup.find_all("p") )
+
+#<p> 태그 한개 검색 
+# print( soup.find("p") )
+
+#<p class="outer-text"> 약간의 검색 조건 
+#print( soup.find_all("p", class_="outer-text") )
+
+#태그는 삭제하고 컨텐츠만 가져오기 
+for tag in soup.find_all("p"):
+    #앞뒤에 태그를 삭제하고 컨텐츠: ~.text 
+    content = tag.text 
+    print( content.strip() )
+
+
+
+
 
 
 
