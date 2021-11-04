a30eb883d4b0fc6d43140f63f49271e31050b151
papasmf1 <papasmf1@gmail.com>
2021-11-04 13:30:45 +0900

    문자열 연습2

--- DemoStr.py
+++ DemoStr.py
@@ -14,3 +14,18 @@ for name in names:
     print("안녕하세요 멋진 가을입니다. {0}님".format(name))
     print("="*40)
 
+#문자열 가공 
+u = "<<<  spam and ham  >>>"
+print(u)
+result = u.strip("<> ")
+print(result)
+result = result.replace("spam", "spam egg")
+print(result)
+lst = result.split() 
+print( lst )
+print("---하나의 문자열로 합치기---")
+print( ":)".join(lst) )
+
+
+
+
